449ef297e0cfea364f34816a7bd5c323703644ef
Mohammed Taysir Alkhatib <medo-11223344@hotmail.com>
2025-07-19 11:21:05 +0300

    Fixing Building Problems

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Portfolio from "./components/portfolio/portfolio";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/footer";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return <>
